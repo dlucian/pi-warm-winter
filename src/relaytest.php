85e99b708eec35ee608bbcc7b77d3508562f427f
Lucian Daniliuc <dlucian@gmail.com>
2017-10-28 11:55:27 +0000

    Relay & Temperature sensor test

---
+++ src/relaytest.php
@@ -0,0 +1,29 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use PhpGpio\Gpio;
+
+$pins = array(17, 18, 22);
+
+echo "Setting up pin 17\n";
+$gpio = new GPIO();
+foreach ($pins as $pin) {
+    $gpio->setup($pin, "out");
+}
+
+while (0) {
+  foreach ($pins as $pin) {
+    echo "Turning on pin $pin\n";
+    $gpio->output($pin, 0);
+    echo "Sleeping!\n";
+    usleep(400000);
+
+    echo "Turning off pin $pin\n";
+    $gpio->output($pin, 1);
+    usleep(400000);
+  }
+}
+
+echo "Unexporting all pins\n";
+$gpio->unexportAll();
